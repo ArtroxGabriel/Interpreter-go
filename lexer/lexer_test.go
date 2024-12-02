56dcea7df10a3ca67958346986c3ea2049014f85
ArtroxGabriel <artroxgabriel@gmail.com>
2024-12-02 18:51:14 -0300

    feat(Token/Lexer): Extend lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,9 +1,8 @@
-package lexer_test
+package lexer
 
 import (
 	"testing"
 
-	"github.com/ArtroxGabriel/interpreter/lexer"
 	"github.com/ArtroxGabriel/interpreter/token"
 )
 
@@ -15,6 +14,17 @@ func TestNextToken(t *testing.T) {
     };
 
     let result = add(five, ten);
+    !-/*5;
+    5 < 10 > 5; 
+
+    if (5 < 10) {
+        return true;
+    } else {
+        return false;
+    }
+
+    10 == 10;
+    10 != 9;
     `
 
 	tests := []struct {
@@ -60,10 +70,54 @@ func TestNextToken(t *testing.T) {
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.TIMES, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+
 		{token.EOF, ""},
 	}
 
-	l := lexer.New(input)
+	l := New(input)
 
 	for i, tt := range tests {
 		tok := l.NextToken()
--- token/token.go
+++ token/token.go
@@ -11,11 +11,25 @@ const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
-	IDENT = "IDENT"
-	INT   = "INT"
+	IDENT  = "IDENT"
+	INT    = "INT"
+	TRUE   = "TRUE"
+	FALSE  = "FALSE"
+	IF     = "IF"
+	ELSE   = "ELSE"
+	RETURN = "RETURN"
 
-	ASSIGN = "ASSIGN"
-	PLUS   = "PLUS"
+	ASSIGN = "="
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	PLUS  = "+"
+	MINUS = "-"
+	TIMES = "*"
+	SLASH = "/"
+	BANG  = "!"
+	LT    = "<"
+	GT    = ">"
 
 	COMMA     = "COMMA"
 	SEMICOLON = "SEMICOLON"
@@ -30,8 +44,13 @@ const (
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"return": RETURN,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
 }
 
 func LookupIdent(ident string) TokenType {
