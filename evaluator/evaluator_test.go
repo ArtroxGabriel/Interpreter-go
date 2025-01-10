2d6f55de971df82279220506e2fcf4accc939c6c
ArtroxGabriel <artroxgabriel@gmail.com>
2025-01-10 09:08:42 -0300

    test: let statement case

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -155,6 +155,22 @@ func TestReturnStatement(t *testing.T) {
 	}
 }
 
+func TestLetStatement(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
 func TestErrorHandling(t *testing.T) {
 	tests := []struct {
 		input           string
@@ -194,6 +210,10 @@ return 1;
 }
 `,
 			"unknown operator: BOOLEAN + BOOLEAN",
+		},
+		{
+			"foobar",
+			"Identifier not found: foobar",
 		},
 	}
 	for _, tt := range tests {
