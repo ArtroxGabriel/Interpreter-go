5342798f1cedee98bb3ccfbe3e90f0c6035f17d4
ArtroxGabriel <artroxgabriel@gmail.com>
2025-01-10 10:07:37 -0300

    test: case calls function

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -253,6 +253,24 @@ func TestFunctionObject(t *testing.T) {
 	}
 }
 
+func TestFunctionApplication(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x) { x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x; }; identity(5);", 5},
+		{"let double = fn(x) { x * 2; }; double(5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5, 5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5 + 5, add(5, 5));", 20},
+		{"fn(x) { x; }(5)", 5},
+	}
+
+	for _, tt := range testCases {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
 func testNullObject(t *testing.T, obj object.Object) {
 	if obj != evaluator.NULL {
 		t.Errorf("Object is not NULL. got=%T (%+v)", obj, obj)
