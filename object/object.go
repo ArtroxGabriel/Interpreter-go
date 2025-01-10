8cf9321af9d7de5eeaa7579e88b956261c05f7ed
ArtroxGabriel <artroxgabriel@gmail.com>
2025-01-10 09:12:37 -0300

    feat: function object

--- object/object.go
+++ object/object.go
@@ -1,7 +1,11 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"strings"
+
+	"github.com/ArtroxGabriel/interpreter/ast"
 )
 
 type ObjectType string
@@ -12,6 +16,7 @@ const (
 	NULL_OBJ         ObjectType = "BOOLEAN"
 	RETURN_VALUE_OBJ ObjectType = "RETURN_VALUE"
 	ERROR_OBJ        ObjectType = "ERROR"
+	FUNCTION_OBJ     ObjectType = "FUNCTION_OBJ"
 )
 
 type Object interface {
@@ -51,3 +56,28 @@ type Error struct {
 
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
