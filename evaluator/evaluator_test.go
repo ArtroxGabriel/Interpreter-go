e74b046331a506c53305558b92c0efdb18ebc2e9
ArtroxGabriel <artroxgabriel@gmail.com>
2025-01-12 10:45:48 -0300

    test: builtin function case

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -315,6 +315,39 @@ func TestStringConcatenation(t *testing.T) {
 	}
 }
 
+func TestBuiltinFunctions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`len("")`, 0},
+		{`len("four")`, 4},
+		{`len("Hello World")`, 11},
+		{`len(1)`, `argument to 'len' not supported. got=INTEGER`},
+		{`len("Hello","World")`, "wrong number of arguments. got=2, want=1"},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t, evaluated, int64(expected))
+		case string:
+			errObj, ok := evaluated.(*object.Error)
+			if !ok {
+				t.Errorf("object is not Error. got=%T (%+v)", evaluated, evaluated)
+				continue
+			}
+
+			if errObj.Message != expected {
+				t.Errorf("Wrong error message. expected=%q. got=%q",
+					expected, errObj.Message)
+			}
+		}
+	}
+}
+
 func testNullObject(t *testing.T, obj object.Object) {
 	if obj != evaluator.NULL {
 		t.Errorf("Object is not NULL. got=%T (%+v)", obj, obj)
