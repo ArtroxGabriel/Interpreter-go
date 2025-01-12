a8cc729e26c85b4c776ce69cd3b2e8f822a706b2
ArtroxGabriel <artroxgabriel@gmail.com>
2025-01-12 10:22:54 -0300

    feat: add infix operator concat string

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -72,7 +72,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return right
 		}
 
-		return evalInfixExpression(node.Operator, left, right)
+		val := evalInfixExpression(node.Operator, left, right)
+		return val
 
 	case *ast.IfExpression:
 		return evalIfExpression(node, env)
@@ -163,9 +164,25 @@ func evalInfixExpression(
 		return nativeBoolToBooleanObject(left != right)
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
+}
+
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s",
+			left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
 
 func evalIntegerInfixExpression(
