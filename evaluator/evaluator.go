3b5196449023d38bd9baa5140693d2e8b3db67c2
ArtroxGabriel <artroxgabriel@gmail.com>
2025-01-13 13:28:11 -0300

    feat: eval array and index array

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -98,6 +98,29 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.StringLiteral:
 		return &object.String{Value: node.Value}
 
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+
+		return &object.Array{Elements: elements}
+
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+
+		return evalIndexExpression(left, index)
+
+	default:
+		return newError("unexpected ast.Node: %#v", node)
 	}
 	return nil
 }
@@ -294,6 +317,32 @@ func evalExpressions(
 	}
 
 	return result
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s",
+			left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	maxIndex := int64(len(arrayObject.Elements) - 1)
+
+	if idx == -1 {
+		return arrayObject.Elements[maxIndex]
+	}
+
+	if idx < 0 || idx > maxIndex {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
 }
 
 func applyFunction(
