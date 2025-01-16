b43e5e6728a07dba4165e907aa8071863e633cfb
ArtroxGabriel <artroxgabriel@gmail.com>
2025-01-16 18:56:01 -0300

    fix: function object constant

--- object/object.go
+++ object/object.go
@@ -17,7 +17,7 @@ const (
 	NULL_OBJ         ObjectType = "BOOLEAN"
 	RETURN_VALUE_OBJ ObjectType = "RETURN_VALUE"
 	ERROR_OBJ        ObjectType = "ERROR"
-	FUNCTION_OBJ     ObjectType = "FUNCTION_OBJ"
+	FUNCTION_OBJ     ObjectType = "FUNCTION"
 	STRING_OBJ       ObjectType = "STRING"
 	BUILTIN_OBJ      ObjectType = "BUILTIN"
 	ARRAY_OBJ        ObjectType = "ARRAY"
